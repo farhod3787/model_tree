2e5cc9a9f0d7d8c1695b50130a2176840fbbbedc
Farhod <marufjonovfarhod@gmail.com>
2021-10-28 14:21:28 +0500

    add cloud mongo

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
-// port = 3000;
-port = 'https://model-tree.herokuapp.com';
+port = 3000;
+// port = 'https://model-tree.herokuapp.com';
 
 // database = 'mongodb://localhost:27017/tree';
 database = 'mongodb+srv://farhod:7Q8SfcHx.F2J.HG@cluster0-uf7cc.mongodb.net/tree?retryWrites=true';
